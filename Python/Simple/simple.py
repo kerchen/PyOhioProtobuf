e63eeb8446b8249bf1f6e67beb9838a03c4e7439
Paul Kerchen <paul@whirlingchair.com>
2017-07-22 14:49:52 -0400

    Start of presentation slides

---
+++ Python/Simple/simple.py
@@ -0,0 +1,31 @@
+import binascii
+import simple_pb2  # protoc appends _pb2, even if proto3!
+
+
+def serialize(t, h):
+    msg = simple_pb2.SensorData()
+    msg.temperature = t
+    msg.humidity = h
+    msg.units = simple_pb2.SensorData.REBEL
+    return msg.SerializeToString()
+
+def deserialize(serialized_data):
+    msg = simple_pb2.SensorData()
+    msg.ParseFromString(serialized_data)
+    t = msg.temperature
+    h = msg.humidity
+    units = msg.units
+    if units == simple_pb2.SensorData.IMPERIAL:
+        t = (t - 32) * 5.0 / 9.0
+    return (t, h)
+
+
+def main():
+    payload = serialize(27.33, 56)
+    print("Payload len: {}".format(len(payload)))
+    print("Payload: " + binascii.hexlify(payload))
+    t, h = deserialize(payload)
+    print("Temperature: {0:.2f} C  Humidity: {1:d} %".format(t, h))
+
+if __name__ == "__main__":
+    main()
